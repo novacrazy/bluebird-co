4c7f70f1c27249977f574d982df303377036ae83
novacrazy <novacrazy@gmail.com>
2015-12-10 04:03:27 -0600

    Refactor index.js a little

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * Created by Aaron on 7/9/2015.
  */
 
-var Promise = require( 'bluebird' );
-var yield_handler = require( './build/yield_handler.js' );
+var Promise     = require( 'bluebird' ),
+    bluebird_co = require( './build/yield_handler.js' );
 
-Promise.coroutine.addYieldHandler( yield_handler.toPromise );
+Promise.coroutine.addYieldHandler( bluebird_co.toPromise );
 
-module.exports = yield_handler;
+module.exports = bluebird_co;
